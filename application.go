4e26aa041a21855a5cfa2fc3be71f54471e60190
double <2637309949@qq.com>
2019-08-31 13:00:59 +0800

    feat: 优化插件

--- application.go
+++ application.go
@@ -13,12 +13,22 @@ import (
 )
 
 // Upload file plugin
-type Upload struct {
-	Path            string
-	PublicURLPrefix string
-	URLPrefix       string
-	Save            func(c *gin.Context, files []map[string]interface{})
-}
+type (
+	Upload struct {
+		Path            string
+		PublicURLPrefix string
+		URLPrefix       string
+		Save            func(c *gin.Context, files []FileInfo)
+	}
+	// FileInfo defined file info
+	FileInfo struct {
+		UID    string
+		Status string
+		Name   string
+		Size   int64
+		URL    string
+	}
+)
 
 // New defined return a Upload with default property
 func New() *Upload {
@@ -30,6 +40,14 @@ func New() *Upload {
 	return up
 }
 
+// AddOptions defined add option
+func (upload *Upload) AddOptions(opts ...Option) *Upload {
+	for _, v := range opts {
+		v.apply(upload)
+	}
+	return upload
+}
+
 // Plugin for bulrush
 func (upload *Upload) Plugin(router *gin.RouterGroup) {
 	router.POST(upload.URLPrefix, func(c *gin.Context) {
@@ -39,23 +57,25 @@ func (upload *Upload) Plugin(router *gin.RouterGroup) {
 				"message": err.Error(),
 			})
 		}
-		ret := make([]map[string]interface{}, 0)
+		ret := make([]FileInfo, 0)
 		for _, files := range form.File {
 			for _, file := range files {
 				filename := filepath.Base(file.Filename)
 				uuid := RandString(32)
 				uuidFileName := RandString(32) + string(filename[len(filename)-len(filepath.Ext(filename)):])
+				size := file.Size
 				if err := c.SaveUploadedFile(file, path.Join(upload.Path, uuidFileName)); err != nil {
 					c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
 						"message": err.Error(),
 					})
 					return
 				}
-				item := map[string]interface{}{
-					"uid":    uuid,
-					"status": "done",
-					"name":   filename,
-					"url":    upload.PublicURLPrefix + "/" + uuidFileName,
+				item := FileInfo{
+					UID:    uuid,
+					Status: "done",
+					Name:   filename,
+					Size:   size,
+					URL:    upload.PublicURLPrefix + "/" + uuidFileName,
 				}
 				ret = append(ret, item)
 			}
---
+++ options.go
@@ -0,0 +1,26 @@
+// Copyright (c) 2018-2020 Double All rights reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file.
+
+package upload
+
+import "github.com/gin-gonic/gin"
+
+// Option defined implement of option
+type (
+	Option func(*Upload) interface{}
+)
+
+// option defined implement of option
+func (o Option) apply(r *Upload) *Upload { return o(r).(*Upload) }
+
+// option defined implement of option
+func (o Option) check(r *Upload) interface{} { return o(r) }
+
+// SaveOption defined save
+func SaveOption(save func(c *gin.Context, files []FileInfo)) Option {
+	return Option(func(r *Upload) interface{} {
+		r.Save = save
+		return r
+	})
+}
